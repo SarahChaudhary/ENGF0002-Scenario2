f5ffce06d902eea2b305ea01786856b41899e10b
Ben Alexander <benjaminalexander747@gmail.com>
2023-03-06 18:11:10 +0000

    create question ui becoming moveable

--- create_question.py
+++ create_question.py
@@ -11,8 +11,7 @@ q_check_valid -> QPushButton
 q_add_question -> QPushButton
 q_finish_quiz -> QPushButton
 q_update -> QPushButton
-q_left_matrix -> list(QLineEdit)
-q_right_matrix -> list(QLineEdit)
+q_left_matrix -> widget containing left matrix
 q_operator_label -> QLabel
 '''
 
@@ -23,26 +22,28 @@ class CreateQuestion(QMainWindow, ui):
         self.setupUi(appwindow)
 
         # this is the 9 matrix input spaces (QLineEdit) collated into a list [QLineEdit]
-        self.q_left_matrix = [self.q1,
-                              self.q2,
-                              self.q3,
-                              self.q4,
-                              self.q5,
-                              self.q6,
-                              self.q7,
-                              self.q8,
-                              self.q9]
+        # self.left_matrix_values = [self.q1,
+        #                            self.q2,
+        #                            self.q3,
+        #                            self.q4,
+        #                            self.q5,
+        #                            self.q6,
+        #                            self.q7,
+        #                            self.q8,
+        #                            self.q9]
 
         # same as above but for the second matrix
-        self.q_right_matrix = [self.q10,
-                               self.q11,
-                               self.q12,
-                               self.q13,
-                               self.q14,
-                               self.q15,
-                               self.q16,
-                               self.q17,
-                               self.q18]
+        # self.right_matrix_values = [self.q10,
+        #                             self.q11,
+        #                             self.q12,
+        #                             self.q13,
+        #                             self.q14,
+        #                             self.q15,
+        #                             self.q16,
+        #                             self.q17,
+        #                             self.q18]
+
+        # QLine objects that make up the left matrix's brackets
 
         self.q_check_valid.clicked.connect(self.check_valid_pressed)
         self.q_auto_generate.clicked.connect(self.auto_generate_pressed)
@@ -50,6 +51,7 @@ class CreateQuestion(QMainWindow, ui):
         self.q_finish_quiz.clicked.connect(self.finish_quiz_pressed)
         self.q_update.clicked.connect(self.update_pressed)
 
+    # these methods below will be called when the corresponding buttons are clicked
     def check_valid_pressed(self):
         pass
 
@@ -71,20 +73,15 @@ class CreateQuestion(QMainWindow, ui):
                               'Determinant': 'Det'}
 
         dropdown_string = self.q_operator_dropdown.currentText()
-        # inverse and determinant will require the box to be moved
+        self.q_operator_label.setText(self.operator_dict[dropdown_string])
+
         if dropdown_string == 'Inverse':
-            self.change_ui_to_inverse()
+            self.q_left_matrix.hide()
 
         elif dropdown_string == 'Determinant':
+            self.q_left_matrix.hide()
             # need to hide the right matrix
-            self.change_ui_to_determinant()
 
         elif dropdown_string == 'Multiply' or dropdown_string == 'Subtraction' or dropdown_string == 'Addition':
-            self.q_operator_label.setText(self.operator_dict[dropdown_string])
+            self.q_left_matrix.show()
             # need to show the left matrix
-
-    def change_ui_to_inverse(self):
-        pass
-
-    def change_ui_to_determinant(self):
-        pass
