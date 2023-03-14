6f79cce6e4f15bb8d7b9c7a701e09329cf0767ce
IZ-code136 <isaacpf12345@gmail.com>
2023-03-14 13:31:01 +0000

    functions

--- matrix.py
+++ matrix.py
@@ -1,3 +1,5 @@
+import numpy as np
+import random
 class Matrix:
     # values is 2d list
     def __init__(self, size, values):
@@ -21,33 +23,41 @@ def matrix_addition(matrix1, matrix2):
 
 
 def matrix_subtraction(matrix1, matrix2):
-    pass
+    return np.subtract(matrix1, matrix2).tolist()
 
 
 def matrix_multiplication(matrix1, matrix2):
-    pass
+    return np.matmul(matrix1, matrix2).tolist()
 
 
 def matrix_dot_product(matrix1, matrix2):
-    pass
+    return np.dot(matrix1, matrix2).tolist()
 
 
 def matrix_determinant(matrix):
-    pass
+    return np.linalg.det(matrix).tolist()
 
 
 def matrix_inverse(matrix):
-    pass
+    return np.linalg.inv(matrix).tolist()
 
 
 def matrix_eigenvalues(matrix):
-    pass
+    return np.linalg.eig(matrix)[0].tolist()
 
 
 def matrix_eigenvectors(matrix):
-    pass
+    return np.linalg.eig(matrix)[1].tolist()
 
+def rand(rows, cols): 
+    arr = []
+    for row in range(rows):
+        arr.append([])
+        for col in range(cols):
+            arr[row].append(random.randint(0,9))
+    return np.array(arr).tolist()
 
+print(matrix_eigenvalues([[1,2,3], [3,2,1], [2,1,3]]))
 # m1 = Matrix(3, [[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 # m2 = Matrix(3, [[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 
