a83e430f57c376a7304e313cfa9804e66b738ca5
Ben Alexander <benjaminalexander747@gmail.com>
2023-03-19 19:01:03 +0000

    beginning doing quiz

--- doing_quiz.py
+++ doing_quiz.py
@@ -9,6 +9,30 @@ class DoingQuiz(QMainWindow, ui):
         super().__init__()
         self.setupUi(appwindow)
 
+        # list of the pyqt matrix values
+        self.left_matrix_values = [
+            self.q1,
+            self.q2,
+            self.q3,
+            self.q4,
+            self.q5,
+            self.q6,
+            self.q7,
+            self.q8,
+            self.q9,
+        ]
+        self.right_matrix_values = [
+            self.q10,
+            self.q11,
+            self.q12,
+            self.q13,
+            self.q14,
+            self.q15,
+            self.q16,
+            self.q17,
+            self.q18,
+        ]
+
         self.q_single_answer.hide()
         self.load_quiz()
 
@@ -32,12 +56,21 @@ class DoingQuiz(QMainWindow, ui):
 
 
     def next_question_clicked(self):
+        # load the values for the next question... ill sort the front end of it
         pass
 
     def previous_question_clicked(self):
+        # same here
         pass
 
     def check_question_clicked(self):
+        # here are the matrix values, as a 1d list of strings
+        left_matrix_values = [i.text() for i in self.left_matrix_values]
+        right_matrix_values = [i.text() for i in self.right_matrix_values]
+
+        # do the calculations on them, may need to convert to 2d array
+        # if it's a unary operation, then use the right matrix values not the left
+        # return true or false for now, ill implement through to front end once done
         pass
 
 
