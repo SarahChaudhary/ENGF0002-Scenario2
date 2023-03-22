8ae3896a21e235415a32fa46c2ad25e8a739c8b3
Sarah <zcabsc5@ucl.ac.uk>
2023-03-22 02:00:54 +0000

    doing_quiz

--- doing_quiz.py
+++ doing_quiz.py
@@ -43,30 +43,75 @@ class DoingQuiz(QMainWindow, ui):
 
     def load_quiz(self):
         # load the quiz from database into here, load the questions as well
+        # connect to the database
+        conn = sqlite3.connect('mydatabase.db')
+        c = conn.cursor()
+
+        # get the quiz name
+        quiz_id = 1  # replace with the actual quiz ID
+        c.execute("SELECT name FROM quizzes WHERE id=?", (quiz_id,))
+        quiz_name = c.fetchone()[0]
+
+        # load the questions for the quiz
+        c.execute("SELECT id, question FROM questions WHERE quiz_id=?", (quiz_id,))
+        questions = c.fetchall()
+
+        # close the database connection
+        conn.close()
+
+        # store the quiz name and questions
+        self.quiz_name = quiz_name
+        self.questions = questions
         pass
 
     def load_question(self):
         # need to load the quiz name into quiz_name variable
-        quiz_name = "quiz name"
-        self.q_quiz_name.setText(quiz_name)
+        #quiz_name = "quiz name"
+        #self.q_quiz_name.setText(quiz_name)
 
         # need to load the question number into question_number variable
-        question_number = "1"
-        self.q_question_number.setText(question_number)
+        #question_number = "1"
+        #self.q_question_number.setText(question_number)
 
         # need to load the username into username variable
-        username = "test"
+        #username = "test"
+        #self.q_by_username.setText('by ' + username)
+        #self.format_matrices()
+        
+        # Get the quiz and question information from the database
+        quiz_id = 1 # Replace with the actual quiz ID
+        question_number = 1 # Replace with the actual question number
+        db = sqlite3.connect('database.db')
+        cursor = db.cursor()
+        cursor.execute("SELECT name FROM quizzes WHERE id=?", (quiz_id,))
+        quiz_name = cursor.fetchone()[0]
+        cursor.execute("SELECT question FROM questions WHERE quiz_id=? AND number=?", (quiz_id, question_number))
+        question_text = cursor.fetchone()[0]
+        cursor.execute("SELECT username FROM users WHERE id=?", (self.user_id,))
+        username = cursor.fetchone()[0]
+        db.close()
+
+        # Update the labels and text fields on the UI
+        self.q_quiz_name.setText(quiz_name)
+        self.q_question_number.setText(str(question_number))
+        self.q_question_text.setText(question_text)
         self.q_by_username.setText('by ' + username)
         self.format_matrices()
 
 
     def next_question_clicked(self):
         # load the values for the next question... ill sort the front end of it. make the values a 1d array of strings
+        self.question_number += 1
+        self.load_question()
+        
         self.format_matrices()
         pass
 
     def previous_question_clicked(self):
         # same here
+        self.question_number -= 1
+        self.load_question()
+        
         self.format_matrices()
         pass
 
